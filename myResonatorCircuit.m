ebd149e9287fa18be7adca1a4e0b4ab52863f3fb
Phzing <hoffman.z@wustl.edu>
2021-11-19 14:36:32 -0600

    myResonatorCircuit and testing

--- myResonatorCircuit.m
+++ myResonatorCircuit.m
@@ -16,5 +16,42 @@
 % Vout - time-series vector representing the output voltage of a circuit
 
 function Vout = myResonatorCircuit(Vin,h)
-Vout = Vin;
+
+Vin_length = size(Vin, 1);
+Vout_seconds = 2;
+
+R = 2.5;
+L = .361716*10^-1;  % tuned L for 440 hz 
+C = .361716*10^-5;  % tuned C for 440 hz
+V_C = 0;
+I=0;
+
+V_time_data = [Vout_seconds/h, 3];
+
+for k=1:Vout_seconds/h        % to generate the data, will be 1:(1/h)*5
+    A = [1, h/C; -h/L, 1-h*R/L];
+    x_k = [V_C, I]';
+    B = [0, h/L]';
+    
+    if k<=Vin_length
+        u_k = Vin(k, 1);
+    else
+        u_k = 0;
+    end
+    
+    x_k_f= A*x_k + B*u_k;
+    V_time_data(k, 1) = I*R;
+    V_time_data(k, 2) = k*h;
+    V_time_data(k, 3) = u_k;
+    V_C = x_k_f(1, 1);
+    I = x_k_f(2, 1);
 end
+
+Vout = V_time_data(:, 1);
+
+% Vout = Vin;
+end
+
+
+
+
---
+++ myResonatorCircuitTesting.m
@@ -0,0 +1,37 @@
+%%
+%Function testing
+
+steps = 192000;
+h = 1/steps;
+f = 440;
+multiply = 5;
+
+Vin = zeros(steps*f*multiply*h, 1);
+for k = 1:steps*f*multiply*h
+    Vin(k, 1) = sin(2*pi*k*h*f);
+end
+
+Vin = ones(1, 1);
+Vout = myResonatorCircuit(Vin, h);
+
+
+
+%%
+playSound(Vin(:, 1), steps);
+pause(1);
+playSound(Vout(:, 1), steps);
+
+
+V_time_data = zeros(size(Vout, 1), 1);
+for k=1:size(Vout, 1)
+    V_time_data(k, 1) = k*h;
+end
+
+figure();
+hold on;
+plot(V_time_data(:, 1),Vout(:, 1), "LineWidth", 2);
+plot(V_time_data([1:steps*multiply*f*h], 1),Vin(:, 1), "LineWidth", 2);
+hold off;
+legend("out", "in");
+
+%x = size(Vin, 1)
